3bbd65cb463c87620c43b82dc48b9ee185cf502b
ingeniouslv <herb@ingeniouslv.com>
2015-02-04 21:19:59 -0800

    Added acf for custom content types
    implements for future content types

--- front-page.php
+++ front-page.php
@@ -1,12 +1,19 @@
-<?php get_header(); ?>
+<?php
+/*
+ * Template Name: Front Page
+ * Description: Template for front page MAC slider
+ */
+  get_header();
+?>
 
   <section>
 	  <div id="component" class="component component-fullwidth fxPressAway">
 		  <ul class="itemwrap">
 			  <li class="first" style="background-image:url('<?php bloginfo('template_directory'); ?>/img/mac-1.jpg'); background-size:cover;">
-			  	<div class="left-text-box slider-text">
-			  		<h1>Metro Arts Council of Southern Nevada</h1>
-				  	<p class="slider-copy">Metro Arts Council is a non-profit that is organized to encourage and strengthen the culture life of Southern Nevada, as well as further the development of the arts and culture.</p>
+				<a class="target-area" href=""></a>
+				<div class="left-text-box slider-text">
+					<h1>Metro Arts Council of Southern Nevada</h1>
+					<p class="slider-copy">Metro Arts Council is a non-profit that is organized to encourage and strengthen the culture life of Southern Nevada, as well as further the development of the arts and culture.</p>
 			  	</div>
 			  </li>
 			  <li style="background-image:url('<?php bloginfo('template_directory'); ?>/img/mac-2.jpg'); background-size:cover;">
@@ -28,5 +35,5 @@
  		  </nav>
 	  </div>
   </section>
-  
+
 <?php get_footer(); ?>
--- functions.php
+++ functions.php
@@ -5,7 +5,10 @@ if ( ! function_exists( 'mac_setup' ) ):
 
 function mac_setup() {
 	add_theme_support( 'html5', array( 'search-form', 'comment-form', 'comment-list', 'gallery', 'caption' ) );
-	add_theme_support( 'post-thumbnails' ); 
+	add_theme_support( 'post-thumbnails' );
 }
 endif;
 add_action( 'after_setup_theme', 'mac_setup' );
+
+include_once( 'inc/acf/acf.php' );
+include_once( 'inc/acf/acf-add-ons/acf-repeater/acf-repeater.php' );
